ae286ecf91cdc4a4d59c940cd63a9d9d145dfd34
Bhavesh Gaikwad <bhavesh.gaikwad@thinkitive.com>
2022-07-11 12:48:14 +0530

    create component title-casting

--- Angular-Assignment-3/assignment3-app/src/app/app.module.ts
+++ Angular-Assignment-3/assignment3-app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TitleCasingComponent } from './title-casing/title-casing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleCasingComponent
   ],
   imports: [
     BrowserModule
