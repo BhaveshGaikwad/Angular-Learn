dbb0c1b56be2f29df31039ea428cd0a5453dc080
Bhavesh Gaikwad <bhavesh.gaikwad@thinkitive.com>
2022-07-12 20:17:31 +0530

    add bootsrap-icon and basic structure.

--- assignment5-app/src/app/zippy/zippy.component.ts
+++ assignment5-app/src/app/zippy/zippy.component.ts
@@ -7,11 +7,18 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ZippyComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    this.openStatus=false;
+  }
 
   ngOnInit(): void {
   }
 
   @Input() title:any;
+  private openStatus:boolean;
 
+  onClick(){
+    this.openStatus= !this.openStatus;
+    console.log(this.openStatus);
+  }
 }
