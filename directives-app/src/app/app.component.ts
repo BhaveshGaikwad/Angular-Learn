4c00d07d730d05aa7b9c55e2eb8c19c66e4f3edd
Bhavesh Gaikwad <bhavesh.gaikwad@thinkitive.com>
2022-07-12 14:07:31 +0530

    add *ngIF

--- directives-app/src/app/app.component.ts
+++ directives-app/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'directives-app';
+
+  ngIfVariable={
+    condition : 1
+  }
 }
--- directives-app/src/app/app.module.ts
+++ directives-app/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgIfDirectivesComponent } from './ng-if-directives/ng-if-directives.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgIfDirectivesComponent
   ],
   imports: [
     BrowserModule
